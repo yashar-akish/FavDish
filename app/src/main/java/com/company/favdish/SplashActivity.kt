6648e45c8b7a097a025b0785d04db2e0e362cc12
yashar-akish <yashar_akish@yahoo.com>
2021-09-03 14:26:19 -0400

    removing ActionBar from splash screen

--- app/src/main/java/com/company/favdish/SplashActivity.kt
+++ app/src/main/java/com/company/favdish/SplashActivity.kt
@@ -1,7 +1,10 @@
 package com.company.favdish
 
-import androidx.appcompat.app.AppCompatActivity
+import android.os.Build
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
 import com.company.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
@@ -11,5 +14,15 @@ class SplashActivity : AppCompatActivity() {
         val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
         setContentView(splashBinding.root)
 
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.insetsController?.hide(WindowInsets.Type.statusBars())
+        } else {
+            @Suppress("DEPRECATION")
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN
+            )
+        }
     }
 }
