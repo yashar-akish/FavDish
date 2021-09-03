7ad4a9dc752631fd586ffeccf3bc40a8a1c2e087
yashar-akish <yashar_akish@yahoo.com>
2021-09-03 18:13:37 -0400

    add and update activity adding lable

---
+++ app/src/main/java/com/company/favdish/view/activities/AddUpdateDishActivity.kt
@@ -0,0 +1,12 @@
+package com.company.favdish.view.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.company.favdish.R
+
+class AddUpdateDishActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_update_dish)
+    }
+}
