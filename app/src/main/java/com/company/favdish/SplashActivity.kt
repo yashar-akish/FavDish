3dfb75fdf82c0043ffb10b93bc347b3a9cb49439
yashar-akish <yashar_akish@yahoo.com>
2021-09-03 14:04:18 -0400

    putting the splash page as first page

--- app/src/main/java/com/company/favdish/SplashActivity.kt
+++ app/src/main/java/com/company/favdish/SplashActivity.kt
@@ -2,10 +2,14 @@ package com.company.favdish
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.company.favdish.databinding.ActivitySplashBinding
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
+
+        val splashBinding: ActivitySplashBinding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(splashBinding.root)
+
     }
 }
