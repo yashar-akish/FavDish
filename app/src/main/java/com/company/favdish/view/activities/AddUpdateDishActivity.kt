43680eff288433bd7a7c58ac236d4c0361155176
yashar-akish <yashar_akish@yahoo.com>
2021-09-13 12:41:41 -0400

    setting up the addUpdateActivity with binding and toolbar and back bottun on toolbar

--- app/src/main/java/com/company/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/company/favdish/view/activities/AddUpdateDishActivity.kt
@@ -3,10 +3,29 @@ package com.company.favdish.view.activities
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.company.favdish.R
+import com.company.favdish.databinding.ActivityAddUpdateDishBinding
 
 class AddUpdateDishActivity : AppCompatActivity() {
+
+    private lateinit var mBinding: ActivityAddUpdateDishBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_add_update_dish)
+        mBinding = ActivityAddUpdateDishBinding.inflate(layoutInflater)
+        setContentView(mBinding.root)
+
+        setupActionBar()
+    }
+
+    /**
+     * setting uo the toolbar
+     */
+    private fun setupActionBar(){
+        setSupportActionBar(mBinding.toolbarAddDishActivity)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true) //allow to have back button on toolbar
+        // adding clickListener to back button:
+        mBinding.toolbarAddDishActivity.setNavigationOnClickListener {
+            onBackPressed()
+        }
     }
 }
