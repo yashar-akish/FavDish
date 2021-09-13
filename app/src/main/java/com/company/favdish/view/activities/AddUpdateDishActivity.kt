04e639dabd5959e340bac3b08aa5cc0de0bd58f0
yashar-akish <yashar_akish@yahoo.com>
2021-09-13 19:46:38 -0400

    function to display the dialog for selecting the image

--- app/src/main/java/com/company/favdish/view/activities/AddUpdateDishActivity.kt
+++ app/src/main/java/com/company/favdish/view/activities/AddUpdateDishActivity.kt
@@ -1,11 +1,13 @@
 package com.company.favdish.view.activities
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
 import com.company.favdish.R
 import com.company.favdish.databinding.ActivityAddUpdateDishBinding
+import com.company.favdish.databinding.DialogCustomImageSelectionBinding
 
 class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
 
@@ -33,14 +35,39 @@ class AddUpdateDishActivity : AppCompatActivity(), View.OnClickListener {
         }
     }
 
+    /**
+     * implement View.OnClickListener
+     */
     override fun onClick(v: View?) {
         if (v != null){
             when (v.id) {
                 R.id.iv_add_dish_image ->{
-                    Toast.makeText(this,"............",Toast.LENGTH_SHORT).show()
+                    // display the dialog
+                    customImageSelectionDialog()
                     return
                 }
             }
         }
+    }
+
+    /**
+     * displaying the dialog to select image
+     */
+    private fun customImageSelectionDialog(){
+        val dialog = Dialog(this)
+        val binding: DialogCustomImageSelectionBinding = DialogCustomImageSelectionBinding.inflate(layoutInflater)
+        dialog.setContentView(binding.root)
+
+        binding.tvCamera.setOnClickListener {
+            Toast.makeText(this, "Camera clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        binding.tvGallery.setOnClickListener {
+            Toast.makeText(this, "Gallery clicked", Toast.LENGTH_SHORT).show()
+            dialog.dismiss()
+        }
+
+        dialog.show()
     }
 }
