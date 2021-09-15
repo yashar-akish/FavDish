9c47b285920984ab1ce050c8d43a14dac4d25a5f
yashar-akish <yashar_akish@yahoo.com>
2021-09-14 21:14:07 -0400

    adapter for recyclerView

---
+++ app/src/main/java/com/company/favdish/view/adapters/CustomListItemAdapter.kt
@@ -0,0 +1,32 @@
+package com.company.favdish.view.adapters
+
+import android.app.Activity
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.company.favdish.databinding.ItemCustomListBinding
+
+class CustomListItemAdapter(
+    private val activity: Activity,
+    private val listItem: List<String>,
+    private val selection: String ) : RecyclerView.Adapter<CustomListItemAdapter.ViewHolder>(){
+
+        class ViewHolder(view: ItemCustomListBinding): RecyclerView.ViewHolder(view.root) {
+            val tvText = view.tvText
+        }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val binding: ItemCustomListBinding =
+            ItemCustomListBinding.inflate(LayoutInflater.from(activity), parent, false)
+        return ViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val item = listItem[position]
+        holder.tvText.text = item
+    }
+
+    override fun getItemCount(): Int {
+        return listItem.size
+    }
+}
