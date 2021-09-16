c9d38147bc77886d7fb297f6d407457dfbd87e0b
yashar-akish <yashar_akish@yahoo.com>
2021-09-16 15:26:22 -0400

    Retrieving data -> Dao

--- app/src/main/java/com/company/favdish/model/database/FavDishDao.kt
+++ app/src/main/java/com/company/favdish/model/database/FavDishDao.kt
@@ -2,11 +2,16 @@ package com.company.favdish.model.database
 
 import androidx.room.Dao
 import androidx.room.Insert
+import androidx.room.Query
 import com.company.favdish.model.entities.FavDish
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavDishDao {
 
     @Insert
     suspend fun insertFavDishDetails(favDish: FavDish)
+
+    @Query("SELECT * FROM FAV_DISHES_TABLE ORDER BY ID")
+    fun getAllDishesList(): Flow<List<FavDish>>
 }
